f43b3269062fc2c2a3c5661976bd2098aef32a9e
Carol Hill <ncwmgrad@aol.com>
2014-05-24 17:51:24 -0400

    First commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function will make a Matrix cache to save costly recomputation
 
-## Write a short comment describing this function
+## First makeCacheMatrix will make the matrix object m
+## Function sets the value of the inverse, and then gets the value of the inverse
+## I used the mean example from class to add cashe and superassignment operator
+
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) m <<-inverse
+  getInverse <- function() m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+ }
 
-}
 
+## The following cacheSolve code will compute and 
+##return the inverse of matrix from makeCacheMatrix
 
-## Write a short comment describing this function
+ cacheSolve <- function(x, ...) {
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+##Get the Inverse from makeCacheMatrix
+##If is null, return from cache the inverse matrix
+   m <- x$getInverse()
+   if(!is.null(m)) {
+     message("getting cached data")
+     return(m)
+   }
+   ##Otherwise calculate the inverse of matrix and setInverse in cache
+   ## Return a matrix that is the inverse of 'x'
+   data <- x$get()
+   m <- inverse(data, ...)
+   x$setInverse(m)
+   m
+ }
